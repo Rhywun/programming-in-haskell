adfae048c3b50e018a21259f30e3da05307fef72
Patrick Huffer <rhywun@rhywun.com>
2018-03-10 19:37:55 -0500

    End of ch09

---
+++ Chapter09/Countdown.hs
@@ -0,0 +1,201 @@
+data Op = Add | Sub | Mul | Div
+
+--
+-- Arithmetic operators
+--
+
+instance Show Op where
+  show Add = "+"
+  show Sub = "-"
+  show Mul = "*"
+  show Div = "/"
+
+-- Decide if the application of an operator to two positive naturals gives another
+-- E.g. valid Sub 2 3 == False
+-- E.g. valid Div 2 3 == False
+valid :: Op -> Int -> Int -> Bool
+-- valid Add _ _ = True
+valid Add x y = x <= y
+valid Sub x y = x > y
+-- valid Mul _ _ = True
+valid Mul x y = x /= 1 && y /= 1 && x <= y
+valid Div x y = y /= 1 && x `mod` y == 0
+
+-- Perform an operation
+-- E.g. apply Add 1 2 == 3
+-- E.g. apply Div 8 2 == 4
+apply :: Op -> Int -> Int -> Int
+apply Add x y = x + y
+apply Sub x y = x - y
+apply Mul x y = x * y
+apply Div x y = x `div` y
+
+--
+-- Numeric expressions
+--
+
+data Expr = Val Int | App Op Expr Expr
+
+e = App Add (Val 1) (App Mul (Val 2) (Val 3))      -- 1 + (2 * 3)
+
+instance Show Expr where
+  show (Val n)     = show n
+  show (App o l r) = brak l ++ show o ++ brak r
+                     where brak (Val n) = show n
+                           brak e       = "(" ++ show e ++ ")"
+
+-- Return the list of values in an expression
+-- E.g. values e == [1,2,3]
+values :: Expr -> [Int]
+values (Val n)     = [n]
+values (App _ l r) = values l ++ values r
+
+-- Evaluate an expression, disallowing non-natural values, and
+-- returning an empty list on failure
+-- E.g. eval e == [7]                                 A valid expression
+-- E.g. eval (App Sub (Val 2) (Val 3)) == []          An invalid expression
+eval :: Expr -> [Int]
+eval (Val n)     = [n | n > 0]
+eval (App o l r) = [apply o x y | x <- eval l, y <- eval r, valid o x y]
+
+--
+-- Combinatorial functions
+--
+
+-- Return all subsequences of a list
+-- E.g. subs [1,2,3] == [[],[3],[2],[2,3],[1],[1,3],[1,2],[1,2,3]]
+subs :: [a] -> [[a]]
+subs []     = [[]]
+subs (x:xs) = yss ++ map (x:) yss
+              where yss = subs xs
+
+-- Return all possible ways of inserting a new element into a list
+-- E.g. interleave 1 [2,3,4] == [[1,2,3,4],[2,1,3,4],[2,3,1,4],[2,3,4,1]]
+interleave :: a -> [a] -> [[a]]
+interleave x []     = [[x]]
+interleave x (y:ys) = (x:y:ys) : map (y:) (interleave x ys)
+
+-- Return all permutations of a list
+-- E.g. perms [1,2,3] == [[1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]
+perms :: [a] -> [[a]]
+perms = foldr (concatMap . interleave) [[]]
+
+-- Return all possible ways of selecting zero or more elements of a list in any order
+-- I.e. map all permutations of all subsequences
+-- E.g. choices [1,2,3] == [[],[3],[2],[2,3],[3,2],[1],[1,3],[3,1],[1,2],[2,1],
+--                          [1,2,3],[2,1,3],[2,3,1],[1,3,2],[3,1,2],[3,2,1]]
+choices :: [a] -> [[a]]
+-- choices = concatMap perms . subs
+choices xs = [zs | ys <- subs xs, zs <- perms ys]                               -- Exercise 1
+
+--
+-- Formalising the problem
+--
+
+ns = [1,3,7,10,25,50] :: [Int]
+e' = App Mul (App Add (Val 1) (Val 50)) (App Sub (Val 25) (Val 10))
+
+-- An expression is a solution for a given list of numbers and a target if
+-- the list of values in the expression is chosen from the list of numbers, and the
+-- expression successfully evaluates to the target
+-- E.g. solution e' ns 765 == True
+solution :: Expr -> [Int] -> Int -> Bool
+solution e ns n = elem (values e) (choices ns) && eval e == [n]
+
+--
+-- Brute-force solution
+--
+
+-- Return all possible ways of splitting a list into two non-empty lists that
+-- append to give the original list
+-- E.g. split [1,2,3,4] == [([1],[2,3,4]),([1,2],[3,4]),([1,2,3],[4])]
+split :: [a] -> [([a], [a])]
+split []     = []
+split [_]    = []
+split (x:xs) = ([x], xs) : [(x:ls, rs) | (ls, rs) <- split xs]
+
+-- Return all possible expressions whose list of values is precisely a given list
+-- E.g. exprs [1,2,3] == [1+(2+3),1-(2+3),1*(2+3),1/(2+3),1+(2-3),1-(2-3),1*(2-3),1/(2-3),
+--                        1+(2*3),1-(2*3),1*(2*3),1/(2*3),1+(2/3),1-(2/3),1*(2/3),1/(2/3),
+--                        (1+2)+3,(1+2)-3,(1+2)*3,(1+2)/3,(1-2)+3,(1-2)-3,(1-2)*3,(1-2)/3,
+--                        (1*2)+3,(1*2)-3,(1*2)*3,(1*2)/3,(1/2)+3,(1/2)-3,(1/2)*3,(1/2)/3]
+exprs :: [Int] -> [Expr]
+exprs []  = []
+exprs [n] = [Val n]
+exprs ns  = [e | (ls, rs) <- split ns,
+                        l <- exprs ls,
+                        r <- exprs rs,
+                        e <- combine l r]
+            where combine l r = [App o l r | o <- ops]
+                  ops = [Add, Sub, Mul, Div]
+
+-- Return all possible expressions made up from a list `ns` that solve for `n`
+-- E.g. solutions ns 765 == [3*((7*(50-10))-25),
+--                           ((7*(50-10))-25)*3,
+--                           3*(((50-10)*7)-25), ...hundreds more...]
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <- choices ns, e <- exprs ns', eval e == [n]]
+
+--
+-- Performance testing
+--
+
+-- All answers print in about 4.5 seconds. Can we do better?
+
+{-
+main :: IO ()
+main = print (solutions ns 765)
+-}
+
+--
+-- Combining generation and evaluation
+--
+
+type Result = (Expr, Int)
+
+-- Return all possible results comprising expressions whose list of values is
+-- precisely a given list
+-- E.g. results [1,2,3] == [(1+(2+3),6),(1*(2+3),5),(1+(2*3),7),(1*(2*3),6),
+--                         ((1+2)+3,6),((1+2)*3,9),((1+2)/3,1),((1*2)+3,5),((1*2)*3,6)]
+results :: [Int] -> [Result]
+results []  = []
+results [n] = [(Val n, n) | n > 0]
+results ns  = [res | (ls, rs) <- split ns,
+                          lx  <- results ls,
+                          ry  <- results rs,
+                          res <- combine' lx ry]
+              where
+                combine' (l, x) (r, y) = [(App o l r, apply o x y) | o <- ops, valid o x y]
+                ops = [Add, Sub, Mul, Div]
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n = [e | ns' <- choices ns, (e, m) <- results ns', m == n]
+
+-- Now it completes in about 1/3 second. Can we still do better??
+
+main :: IO ()
+main = print (solutions' ns 765)
+
+--
+-- Exploiting algebraic properties
+--
+
+-- See modifications to `valid` above
+
+-- Completes in 0.06 seconds!
+
+--
+-- Exercises
+--
+
+-- 1
+-- See above
+
+-- 2
+-- PASS
+
+-- 3
+-- PASS
+
+-- 4
+e4 = length $ exprs ns
