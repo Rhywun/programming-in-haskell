28a95ba6c3c8ec4c276590ea5965b1cc9ceb9b8e
Patrick Huffer <rhywun@rhywun.com>
2018-05-13 10:41:04 -0400

    End of Chapter02

--- Chapter02/Scratch.hs
+++ Chapter02/Scratch.hs
@@ -4,6 +4,11 @@ double x = x + x
 
 quadruple x = double (double x)
 
-factorial n = product [1..n]
+factorial n = product [1 .. n]
 
 average ns = sum ns `div` length ns
+
+a = b + c
+  where
+    b = 1
+    c = 2
