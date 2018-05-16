3ecb915de01d822cbfc1a3a9a9b223fa8bddbd77
Patrick Huffer <rhywun@rhywun.com>
2018-05-15 22:46:34 -0400

    Middle of Chapter07

--- Chapter07/Scratch.hs
+++ Chapter07/Scratch.hs
@@ -1,7 +1,8 @@
 module Chapter07.Scratch where
 
+--
 -- 7.1 - Basic concepts
-
+--
 -- Due to currying, this...
 add :: Integer -> Integer -> Integer
 add x y = x + y
@@ -10,32 +11,54 @@ add x y = x + y
 add' :: Integer -> Integer -> Integer
 add' = \x -> (\y -> x + y)
 
--- A function that takes a function as an argument
+-- A function that takes a function as an argument:
+{-
+twice (* 2) 3 -- 12
+twice reverse "hello" -- "hello"
+-}
 twice :: (a -> a) -> a -> a
 twice f x = f (f x)
 
-quadruple = twice (*2)
+{-
+quadruple 4 -- 16
+-}
+quadruple = twice (* 2)
 
+--
 -- 7.2 - Processing lists
+--
+m1 = map (+ 1) [1, 3, 5, 7] -- [2,4,6,8]
 
-m1 = map (+1) [1, 3, 5, 7]
-m2 = map even [1, 2, 3, 4]
-m3 = map reverse ["abc", "def", "ghi"]
-m4 = map (map (+1)) [[1, 2, 3], [4, 5]]
+m2 = map even [1, 2, 3, 4] -- [False,True,False,True]
 
-f1 = filter even [1..10]
-f2 = filter (> 5) [1..10]
+m3 = map reverse ["abc", "def", "ghi"] -- ["cba","fed","ihg"]
 
+m4 = map (map (+ 1)) [[1, 2, 3], [4, 5]] -- ["cba","fed","ihg"]
+
+f1 = filter even [1 .. 10] -- [2,4,6,8,10]
+
+f2 = filter (> 5) [1 .. 10] -- [6,7,8,9,10]
+
+f3 = filter (/= ' ') "abc def ghi" -- "abcdefghi"
+
+-- Combining `map` and `filter`
+--
 -- Returns the sum of the squares of even integers from list ns
+{-
+sumsqreven [1..10]
+--> 4 + 16 + 36 + 64 + 100
+--> 220
+-}
 sumsqreven :: [Int] -> Int
-sumsqreven ns = sum (map (^2) (filter even ns))
+sumsqreven ns = sum (map (^ 2) (filter even ns))
 
+--
 -- 7.3 - The foldr function
-
-sum' :: Num a => [a] -> a           -- Note that the type annotation is required here...
+--
+sum' :: Num a => [a] -> a -- Note that the type annotation is required here...
 sum' = foldr (+) 0
 
-sum'' xs = foldr (+) 0 xs           -- ...but not here
+sum'' xs = foldr (+) 0 xs -- ...but not here
 
 length' :: [a] -> Int
 length' = foldr (\_ n -> 1 + n) 0
@@ -48,7 +71,6 @@ reverse' :: [a] -> [a]
 reverse' = foldr snoc []
 
 -- 7.4 - The foldl function
-
 length'' :: [a] -> Int
 length'' = foldl (\n _ -> n + 1) 0
 
@@ -56,16 +78,15 @@ reverse'' :: [a] -> [a]
 -- reverse'' = foldl (\xs x -> x:xs) []
 reverse'' = foldl (flip (:)) []
 
+--
 -- 7.5 - The composition operator
-
+--
 -- This definition makes it clear that a function is returned:
 {-
 (.) :: (b -> c) -> (a -> b) -> a -> c
 f . g = \x -> f (g x)
 -}
-
 -- We can now rewrite some function definitions more simply:
-
 -- odd n = not (even n)
 odd' = not . even
 
@@ -73,14 +94,12 @@ odd' = not . even
 twice' f = f . f
 
 -- sumsqreven ns = sum (map (^2) (filter even ns))
-sumsqreven' = sum . map (^2) . filter even
+sumsqreven' = sum . map (^ 2) . filter even
 
 -- The composition of a list of functions uses `id` as the identity:
 compose :: [a -> a] -> (a -> a)
 compose = foldr (.) id
-
 -- 7.6 - Binary string transmitter
 -- See Binary.hs
-
 -- 7.7 - Voting algorithms
 -- See Voting.hs
