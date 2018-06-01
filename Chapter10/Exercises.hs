cb51a96717d3ba8c69d010926a78ab297379c6c4
Patrick Huffer <rhywun@rhywun.com>
2018-05-31 20:39:02 -0400

    End of Chapter10

--- Chapter10/Exercises.hs
+++ Chapter10/Exercises.hs
@@ -1,12 +1,12 @@
 module Chapter10.Exercises where
 
-import Data.Char
-import System.IO
+import           Data.Char
+import           System.IO
 
 -- 1
 
 putStr' :: String -> IO ()
-putStr' cs = sequence_ [putChar c | c <- cs]
+putStr' cs = sequence_ [ putChar c | c <- cs ]
 
 -- 2
 -- See Nim.hs
@@ -19,42 +19,47 @@ putStr' cs = sequence_ [putChar c | c <- cs]
 
 -- Prompt for a digit and read a single character, with error checking
 getDigit :: String -> IO Int
-getDigit prompt = do putStr prompt
-                     x <- getChar
-                     putChar '\n'
-                     if isDigit x then
-                       return (digitToInt x)
-                     else
-                       do putStrLn "ERROR: Invalid digit"
-                          getDigit prompt
+getDigit prompt = do
+  putStr prompt
+  x <- getChar
+  putChar '\n'
+  if isDigit x
+    then return (digitToInt x)
+    else do
+      putStrLn "ERROR: Invalid digit"
+      getDigit prompt
 
 adder :: IO ()
-adder = do hSetBuffering stdout NoBuffering
-           n <- getDigit "How many numbers? "
-           ns <- sequence [getDigit ("Number " ++ show i ++ "? ") | i <- [1..n]]
-           putStr "The total is "
-           print $ sum ns
+adder = do
+  hSetBuffering stdout NoBuffering
+  n  <- getDigit "How many numbers? "
+  ns <- sequence [ getDigit ("Number " ++ show i ++ "? ") | i <- [1 .. n] ]
+  putStr "The total is "
+  print $ sum ns
 
 -- 6
 
 readLine :: IO String
-readLine = do hSetBuffering stdout NoBuffering
-              x <- getCh
-              if x == '\n' then
-                do putChar x
-                   return []
-              else if x == '\DEL' then
-                do putChar '\DEL'
-                   putChar '\b'
-                   xs <- readLine
-                   return xs                    -- So close...
-                                                -- But how do I discard the deleted chars??
-              else
-                do putChar x
-                   xs <- readLine
-                   return (x:xs)
-           where getCh :: IO Char
-                 getCh = do hSetEcho stdin False
-                            x <- getChar
-                            hSetEcho stdin True
-                            return x
+readLine = do
+  hSetBuffering stdout NoBuffering
+  x <- getCh
+  if x == '\n'
+    then do
+      putChar x
+      return []
+    else if x == '\DEL'
+      then do
+        putChar '\DEL'
+        putChar '\b'
+        readLine               -- So close... but how do I discard the deleted chars??
+      else do
+        putChar x
+        xs <- readLine
+        return (x : xs)
+ where
+  getCh :: IO Char
+  getCh = do
+    hSetEcho stdin False
+    x <- getChar
+    hSetEcho stdin True
+    return x
--- Chapter10/Life.hs
+++ Chapter10/Life.hs
@@ -11,26 +11,31 @@ type Pos = (Int, Int)
 
 -- Display a string at a given position
 writeAt :: Pos -> String -> IO ()
-writeAt p xs = do goto p
-                  putStr xs
-               where goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+writeAt p xs = do
+  goto p
+  putStr xs
+  where goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
 
 --
 -- Game of life
 --
 
-width = 10 :: Int
-height = 10 :: Int
+width :: Int
+width = 10
+
+height :: Int
+height = 10
 
 -- A board is a list of positions at which there is a living cell
 type Board = [Pos]
 
 -- An example initial board
-glider = [(4,2),(2,3),(4,3),(3,4),(4,4)] :: Board
+glider :: Board
+glider = [(4, 2), (2, 3), (4, 3), (3, 4), (4, 4)]
 
 -- Display a board on the screen
 showCells :: Board -> IO ()
-showCells board = sequence_ [writeAt pos "O" | pos <- board]
+showCells board = sequence_ [ writeAt pos "O" | pos <- board ]
 
 -- Is this cell alive?
 isAlive :: Board -> Pos -> Bool
@@ -42,10 +47,18 @@ isEmpty board pos = not (isAlive board pos)
 
 -- Return the neighbors of a position
 neighbors :: Pos -> [Pos]
-neighbors (x,y) = map wrap [(x-1,y-1), (x,y-1), (x+1,y-1), (x-1,y),
-                            (x+1,y), (x-1,y+1), (x,y+1), (x+1,y+1)]
-                  where wrap (x,y) = (((x-1) `mod` width) + 1,
-                                      ((y-1) `mod` height) + 1)
+neighbors (x, y) = map
+  wrap
+  [ (x - 1, y - 1)
+  , (x    , y - 1)
+  , (x + 1, y - 1)
+  , (x - 1, y)
+  , (x + 1, y)
+  , (x - 1, y + 1)
+  , (x    , y + 1)
+  , (x + 1, y + 1)
+  ]
+  where wrap (x', y') = (((x' - 1) `mod` width) + 1, ((y' - 1) `mod` height) + 1)
 
 -- Count number of live neighbors of a position
 liveNeighbors :: Board -> Pos -> Int
@@ -53,15 +66,20 @@ liveNeighbors board = length . filter (isAlive board) . neighbors
 
 -- List the survivors, those positions on the board that have 2 or 3 living neighbors
 survivors :: Board -> [Pos]
-survivors board = [pos | pos <- board, liveNeighbors board pos `elem` [2, 3]]
+survivors board = [ pos | pos <- board, liveNeighbors board pos `elem` [2, 3] ]
 
 -- List the upcoming births, those positions on the board that are empty and
 -- have 3 live neighbors
 births :: Board -> [Pos]
-births board = [pos | pos <- rmDups (concatMap neighbors board),
-                             isEmpty board pos, liveNeighbors board pos == 3]
-               where rmDups []     = []
-                     rmDups (x:xs) = x : rmDups (filter (/= x) xs)
+births board =
+  [ pos
+  | pos <- rmDups (concatMap neighbors board)
+  , isEmpty board pos
+  , liveNeighbors board pos == 3
+  ]
+ where
+  rmDups []       = []
+  rmDups (x : xs) = x : rmDups (filter (/= x) xs)
 
 -- Return the board's next generation
 nextGen :: Board -> Board
@@ -69,11 +87,12 @@ nextGen board = survivors board ++ births board
 
 -- Implement the game of life
 life :: Board -> IO ()
-life board = do cls
-                showCells board
-                wait 500000
-                life $ nextGen board
-             where wait n = sequence_ [return () | _ <- [1..n]]
+life board = do
+  cls
+  showCells board
+  wait (500000 :: Integer)
+  life $ nextGen board
+  where wait n = sequence_ [ return () | _ <- [1 .. n] ]
 
 main :: IO ()
 main = life glider
