7ef12e55fb380a98b124ffe386affad9739f92cf
Patrick Huffer <rhywun@rhywun.com>
2018-06-14 21:00:42 -0400

    Middle of Chapter13

--- Chapter13/Scratch.hs
+++ Chapter13/Scratch.hs
@@ -5,6 +5,7 @@ module Chapter13.Scratch where
 
 import           Control.Applicative
 import           Data.Char
+import           System.IO
 
 --
 -- 13.3 - Basic definitions
@@ -325,4 +326,133 @@ eval xs = case parse expr xs of
 --
 -- 13.9 - Calculator
 --
+
+-- IO utilities
+
+getCh :: IO Char
+getCh = do
+  hSetEcho stdin False
+  x <- getChar
+  hSetEcho stdin True
+  return x
+
+beep :: IO ()
+beep = putStr "\BEL" -- Doesn't do anything
+
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+writeAt :: (Int, Int) -> String -> IO ()
+writeAt p xs = do
+  goto p
+  putStr xs
+
+goto :: (Int, Int) -> IO ()
+goto (x, y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+-- Calculator
+
+box :: [String]
+box =
+  [ "┌───────────────┐"
+  , "│               │"
+  , "├───┬───┬───┬───┤"
+  , "│ Q │ C │ D │ = │"
+  , "├───┼───┼───┼───┤"
+  , "│ 1 │ 2 │ 3 │ + │"
+  , "├───┼───┼───┼───┤"
+  , "│ 4 │ 5 │ 6 │ - │"
+  , "├───┼───┼───┼───┤"
+  , "│ 7 │ 8 │ 9 │ * │"
+  , "├───┼───┼───┼───┤"
+  , "│ 0 │ ( │ ) │ / │"
+  , "└───┴───┴───┴───┘"
+  ]
+
+buttons :: String
+buttons = standard ++ extra
+ where
+  standard = "qcd=123+456-789*0()/"
+  extra    = "QCD \ESC\BS\DEL\n"
+
+showBox :: IO ()
+showBox = sequence_ [ writeAt (1, y) b | (y, b) <- zip [1 ..] box ]
+
+display :: String -> IO ()
+display xs = do
+  writeAt (3, 2) (replicate 13 ' ')
+  writeAt (3, 2) (reverse (take 13 (reverse xs)))
+
+-- Display the string xs, then get a character and process it if valid
+calc :: String -> IO ()
+calc xs = do
+  display xs
+  c <- getCh
+  if c `elem` buttons
+    then process c xs
+    else do
+      beep
+      calc xs
+
+process :: Char -> String -> IO ()
+process c xs | c `elem` "qQ\ESC"    = quit
+             | c `elem` "dD\BS\DEL" = delete xs
+             | c `elem` "=\n"       = evaluate xs
+             | c `elem` "cC"        = clear
+             | otherwise            = press c xs
+
+quit :: IO ()
+quit = goto (1, 14) -- terminates here
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+evaluate :: String -> IO ()
+evaluate xs = case parse expr xs of
+  [(n, [])] -> calc (show n)
+  _         -> do
+    beep
+    calc xs
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = do
+  cls
+  showBox
+  clear
+
+--
+-- 13.11 - Exercises
+--
+
+-- 1
+
+{-
+parse comment "beep"               -- []
+parse comment "-- i'm a comment\n" -- [((),"")]
+-}
+comment :: Parser ()
+comment = do
+  _ <- string "--"
+  _ <- many (satisfy (/= '\n'))
+  _ <- string "\n"
+  return ()
+
+-- 2
+-- PASS
+
+-- 3
+-- PASS
+
+-- 4
+-- ?
+
+-- 5
+-- ?
 
