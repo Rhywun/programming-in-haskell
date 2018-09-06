5e6fd1e568de2e89ee8fba4bffecbf32e16e48f5
Patrick Huffer <rhywun@rhywun.com>
2018-09-06 13:57:04 -0400

    Added stack files, updates to Chapter01

--- Chapter01/Scratch.hs
+++ Chapter01/Scratch.hs
@@ -2,17 +2,29 @@ module Chapter01.Scratch where
 
 double x = x + x
 
-sum' []     = 0
-sum' (n:ns) = n + sum ns
+{-
+sum' [1,2,3] -- 6
+-}
+sum' []       = 0
+sum' (n : ns) = n + sum ns
 
-qsort [] = []
-qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
-  where
-    smaller = [a | a <- xs, a <= x]
-    larger = [b | b <- xs, b > x]
+{-
+qsort [3,5,1,4,2]   -- [1,2,3,4,5]
+qsort "Mississippi" -- "Miiiippssss"
+-}
+qsort []       = []
+qsort (x : xs) = qsort smaller ++ [x] ++ qsort larger
+ where
+  smaller = [ a | a <- xs, a <= x ]
+  larger  = [ b | b <- xs, b > x ]
 
-seqn [] = return []
-seqn (act:acts) = do
-  x <- act
+{-
+seqn [getChar, getChar, getChar]
+  > abc
+  -- "abc"
+-}
+seqn []           = return []
+seqn (act : acts) = do
+  x  <- act
   xs <- seqn acts
   return (x : xs)
